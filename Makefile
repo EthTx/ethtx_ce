00ce6b9e65fc19d1c4383893c8a1be89094bd971
Karol Chojnowski <karolchojnowski95@gmail.com>
2021-08-03 12:32:40 +0200

    delete deploy, add make command for git version

--- Makefile
+++ Makefile
@@ -6,6 +6,10 @@ help:
 build-image:  ## Build all docker images
 	docker build -t ethtx_ce .
 
+get-git-version: ## Get git version and save it to file
+	chmod +x ./scripts/git_version_for_docker.sh
+	./scripts/git_version_for_docker.sh
+
 run-database:  ## Run only a local database required for local development
 	docker-compose up -d mongo mongo-express
 
--- scripts/git_version_for_docker.sh
+++ scripts/git_version_for_docker.sh
@@ -1,8 +1,12 @@
 #! /usr/bin/env bash
 
+# Execute in root project directory
 # Exit in case of error
 set -e
 
+path=tmp
+file=git_version
+dest_dir="${path}/${file}"
 
 remote_url_git=$(git config --get remote.origin.url)
 remote_url=${remote_url_git::-4}
@@ -12,4 +16,4 @@ sha=$(git rev-parse HEAD)
 url_sha="${remote_url},${sha}"
 
 # return
-echo "$url_sha"
+echo "$url_sha" >$dest_dir
