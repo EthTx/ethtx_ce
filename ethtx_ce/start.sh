846dc3c12dded4b32be5b08c6ed07279ae302ff1
Karol Chojnowski <karolchojnowski95@gmail.com>
2022-03-08 16:47:45 +0100

    update deploy

--- ethtx_ce/start.sh
+++ ethtx_ce/start.sh
@@ -2,4 +2,8 @@
 set -e
 
 # Start Gunicorn
+echo "Starting Gunicorn ..."
+echo "GUNICORN_CONF: $GUNICORN_CONF"
+echo "APP_MODULE: $APP_MODULE"
+
 exec pipenv run gunicorn -k egg:meinheld#gunicorn_worker -c "$GUNICORN_CONF" "$APP_MODULE"
