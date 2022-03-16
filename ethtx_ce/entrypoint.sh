15a7d46a1393efc4380cfff4162c2fcae15ceb22
Karol Chojnowski <karolchojnowski95@gmail.com>
2022-03-16 17:00:04 +0100

    Remove useless stuff, fix front typo

--- ethtx_ce/entrypoint.sh
+++ ethtx_ce/entrypoint.sh
@@ -8,15 +8,11 @@ if [ -f /app/app/wsgi.py ]; then
 elif [ -f /app/wsgi.py ]; then
     DEFAULT_MODULE_NAME=wsgi
 fi
-echo "DEFAULT_MODULE_NAME: $DEFAULT_MODULE_NAME"
 
 MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
 VARIABLE_NAME=${VARIABLE_NAME:-app}
-echo "MODULE_NAME: $MODULE_NAME"
-echo "VARIABLE_NAME: $VARIABLE_NAME"
 
 export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
-echo "APP_MODULE: $APP_MODULE"
 
 if [ -f /app/gunicorn_conf.py ]; then
     DEFAULT_GUNICORN_CONF=/app/gunicorn_conf.py
@@ -26,6 +22,5 @@ else
     DEFAULT_GUNICORN_CONF=/gunicorn_conf.py
 fi
 export GUNICORN_CONF=${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}
-echo "GUNICORN_CONF: $GUNICORN_CONF"
 
 exec "$@"
