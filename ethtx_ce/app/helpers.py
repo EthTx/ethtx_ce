8e952eb773c94f008d28a15e759656423a343dab
Karol Chojnowski <karolchojnowski95@gmail.com>
2022-05-18 13:46:58 +0200

    Add new `info` endpoint + other small upgrades (#112)
    - Added `info` endpoint with ethtx/ethtx_ce versionn
    - refactored `deps`
    - updated `README.md`
    - updated `black` version
    - changed the application name for each component
    - added `get_latest_ethtx_version` function (get version from Pypi)

--- ethtx_ce/app/helpers.py
+++ ethtx_ce/app/helpers.py
@@ -12,10 +12,14 @@
 
 import importlib
 import logging
+import os
 import pkgutil
-from typing import Any, List
+from typing import Any, List, Tuple, Optional
 
+import pkg_resources
+import requests
 from flask import Blueprint, Flask
+from git import Repo
 
 log = logging.getLogger(__name__)
 
@@ -59,3 +63,65 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+
+
+def read_ethtx_versions(app: Flask) -> None:
+    """Read ethtx and ethtx_ce versions."""
+    ethtx_version = pkg_resources.get_distribution("ethtx").version
+
+    try:
+        remote_url, sha = _get_version_from_git()
+    except Exception:
+        remote_url, sha = _get_version_from_docker()
+
+    ethtx_ce_version = f"{_clean_up_git_link(remote_url)}/tree/{sha}"
+
+    log.info(
+        "%s: EthTx version: %s. EthTx CE version: %s",
+        app.name,
+        ethtx_version,
+        ethtx_ce_version,
+    )
+
+    app.config["ethtx_version"] = ethtx_version
+    app.config["ethtx_ce_version"] = ethtx_ce_version
+
+
+def get_latest_ethtx_version() -> str:
+    """Get latest EthTx version."""
+    package = "EthTx"
+    response = requests.get(f"https://pypi.org/pypi/{package}/json")
+
+    if response.status_code != 200:
+        log.warning("Failed to get latest EthTx version from PyPI")
+        return ""
+
+    log.info("Latest EthTx version: %s", response.json()["info"]["version"])
+    return response.json()["info"]["version"]
+
+
+def _get_version_from_git() -> Tuple[str, str]:
+    """Get EthTx CE version from .git"""
+    repo = Repo(__file__, search_parent_directories=True)
+
+    remote_url = repo.remote("origin").url
+    sha = repo.head.commit.hexsha
+    short_sha = repo.git.rev_parse(sha, short=True)
+
+    return remote_url, short_sha
+
+
+def _get_version_from_docker() -> Tuple[str, str]:
+    """Get EthTx CE version from env."""
+    return os.getenv("GIT_URL", ""), os.getenv("GIT_SHA", "")
+
+
+def _clean_up_git_link(git_link: str) -> str:
+    """Clean up git link, delete .git extension, make https url."""
+    if "@" in git_link:
+        git_link.replace(":", "/").replace("git@", "https://")
+
+    if git_link.endswith(".git"):
+        git_link = git_link[:-4]
+
+    return git_link
